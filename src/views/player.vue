062fbf532224bf0c02da7515765e953ff730ba0e
pengyh <l>
2019-10-23 10:39:31 +0800

    优化直播

--- src/views/player.vue
+++ src/views/player.vue
@@ -4,7 +4,7 @@
             <el-button type="primary" @click="keepLiveVideo">开始直播</el-button>
             <el-button type="primary" @click="stopLiveVideo">停止直播</el-button>
         </div>
-        <video ref="player" id="video1" width="640" height="480" controls @pause="handlePause" @play="handlePlay"></video>
+        <video ref="player" id="video1" width="640" height="480" poster="http://pic34.nipic.com/20131030/2455348_194508648000_2.jpg" controls @pause="handlePause" @play="handlePlay"></video>
     </div>
 </template>
 <script>
@@ -19,10 +19,10 @@ export default {
     },
     methods: {
         keepLiveVideo() {
-            if (Wfs.isSupported()) {
+            if (Wfs.isSupported() && this.player === null) {
                 this.player = new Wfs()
                 this.player.attachMedia(this.$refs.player, 'ch1', 'H264Raw', 'play2/2222/2', '112.5.154.242:61161');
-                
+
             }
         },
         stopLiveVideo() {
@@ -45,7 +45,7 @@ export default {
                     this.player.attachMedia(this.$refs.player, 'ch1', 'H264Raw', 'play2/2222/2', '112.5.154.242:61161');
                 }
             }
-        }
+        },
     }
 }
 </script>
